e741ce5a7fe6fdf1bd72304b420b4aa3be0d2951
ClearAspect <roanmason@live.ca>
2025-01-28 10:24:08 -0500

    fix(lualine): incorrect colors

--- lua/lualine/themes/onehalflight.lua
+++ lua/lualine/themes/onehalflight.lua
@@ -4,39 +4,39 @@ local onehalflight = {}
 
 onehalflight.normal = {
 	a = { bg = C.blue, fg = C.bg, gui = "bold" },
-	b = { bg = C.black, fg = C.blue },
+	b = { bg = C.cursor_line, fg = C.blue },
 	c = { bg = C.bg, fg = C.fg },
 }
 
 onehalflight.insert = {
 	a = { bg = C.green, fg = C.bg, gui = "bold" },
-	b = { bg = C.black, fg = C.green },
+	b = { bg = C.cursor_line, fg = C.green },
 }
 
 onehalflight.visual = {
 	a = { bg = C.blue, fg = C.bg, gui = "bold" },
-	b = { bg = C.black, fg = C.blue },
+	b = { bg = C.cursor_line, fg = C.blue },
 }
 
 onehalflight.replace = {
 	a = { bg = C.yellow, fg = C.bg, gui = "bold" },
-	b = { bg = C.black, fg = C.yellow },
+	b = { bg = C.cursor_line, fg = C.yellow },
 }
 
 onehalflight.command = {
 	a = { bg = C.red, fg = C.bg, gui = "bold" },
-	b = { bg = C.black, fg = C.red },
+	b = { bg = C.cursor_line, fg = C.red },
 }
 
 onehalflight.terminal = {
 	a = { bg = C.red, fg = C.bg, gui = "bold" },
-	b = { bg = C.black, fg = C.red },
+	b = { bg = C.cursor_line, fg = C.red },
 }
 
 
 onehalflight.inactive = {
 	a = { bg = C.bg, fg = C.blue },
-	b = { bg = C.bg, fg = C.black, gui = "bold" },
+	b = { bg = C.cursor_line, fg = C.black, gui = "bold" },
 	c = { bg = C.bg, fg = C.black },
 }
 
