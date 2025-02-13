0beab600fe12a634a1591304fd60054b3be7def7
ClearAspect <roanmason@live.ca>
2025-02-12 21:05:42 -0500

    fix(lualine)

--- lua/lualine/themes/onehalfdark.lua
+++ lua/lualine/themes/onehalfdark.lua
@@ -4,39 +4,44 @@ local onehalfdark = {}
 
 onehalfdark.normal = {
 	a = { bg = C.blue, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.blue },
-	c = { bg = C.bg, fg = C.fg },
+	b = { bg = C.non_text, fg = C.blue },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalfdark.insert = {
 	a = { bg = C.green, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.green },
+	b = { bg = C.non_text, fg = C.green },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalfdark.visual = {
 	a = { bg = C.blue, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.blue },
+	b = { bg = C.non_text, fg = C.blue },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalfdark.replace = {
 	a = { bg = C.yellow, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.yellow },
+	b = { bg = C.non_text, fg = C.yellow },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalfdark.command = {
 	a = { bg = C.red, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.red },
+	b = { bg = C.non_text, fg = C.red },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalfdark.terminal = {
 	a = { bg = C.red, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.red },
+	b = { bg = C.non_text, fg = C.red },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 
 onehalfdark.inactive = {
 	a = { bg = C.bg, fg = C.blue },
-	b = { bg = C.cursor_line, fg = C.black, gui = "bold" },
+	b = { bg = C.non_text, fg = C.black, gui = "bold" },
 	c = { bg = C.bg, fg = C.black },
 }
 
--- lua/lualine/themes/onehalflight.lua
+++ lua/lualine/themes/onehalflight.lua
@@ -4,39 +4,43 @@ local onehalflight = {}
 
 onehalflight.normal = {
 	a = { bg = C.blue, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.blue },
-	c = { bg = C.bg, fg = C.fg },
+	b = { bg = C.non_text, fg = C.blue },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalflight.insert = {
 	a = { bg = C.green, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.green },
+	b = { bg = C.non_text, fg = C.green },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalflight.visual = {
 	a = { bg = C.blue, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.blue },
+	b = { bg = C.non_text, fg = C.blue },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalflight.replace = {
 	a = { bg = C.yellow, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.yellow },
+	b = { bg = C.non_text, fg = C.yellow },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalflight.command = {
 	a = { bg = C.red, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.red },
+	b = { bg = C.non_text, fg = C.red },
+	c = { bg = C.non_text, fg = C.fg },
 }
 
 onehalflight.terminal = {
 	a = { bg = C.red, fg = C.bg, gui = "bold" },
-	b = { bg = C.cursor_line, fg = C.red },
+	b = { bg = C.non_text, fg = C.red },
+	c = { bg = C.non_text, fg = C.fg },
 }
-
 
 onehalflight.inactive = {
 	a = { bg = C.bg, fg = C.blue },
-	b = { bg = C.cursor_line, fg = C.black, gui = "bold" },
+	b = { bg = C.non_text, fg = C.black, gui = "bold" },
 	c = { bg = C.bg, fg = C.black },
 }
 
