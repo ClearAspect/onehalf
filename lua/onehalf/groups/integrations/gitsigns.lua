cfc022a89ae7f3035f90f230524f05f7b0cc3c74
ClearAspect <roanmason@live.ca>
2025-03-24 11:20:32 -0400

    fix(gitsigns): missing highlights

--- lua/onehalf/groups/integrations/gitsigns.lua
+++ lua/onehalf/groups/integrations/gitsigns.lua
@@ -16,6 +16,21 @@ function M.get(C, O)
 		GitSignsDeleteNr = { fg = C.red, },
 		GitSignsDeleteLn = { bg = U.blend_colors(C.red, C.bg, 0.25) },
 		GitSignsDeleteCul = { bg = C.red },
+
+
+		GitSignsStagedAdd = { fg = C.green, },
+		GitSignsStagedAddNr = { fg = C.green, },
+		GitSignsStagedAddLn = { bg = U.blend_colors(C.green, C.bg, 0.25) },
+		GitSignsStagedAddCul = { bg = C.green },
+		GitSignsStagedChange = { fg = C.yellow, },
+		GitSignsStagedChangeNr = { fg = C.yellow, },
+		GitSignsStagedChangeLn = { bg = U.blend_colors(C.yellow, C.bg, 0.25) },
+		GitSignsStagedChangeCul = { bg = C.yellow },
+		GitSignsStagedDelete = { fg = C.red, },
+		GitSignsStagedDeleteNr = { fg = C.red, },
+		GitSignsStagedDeleteLn = { bg = U.blend_colors(C.red, C.bg, 0.25) },
+		GitSignsStagedDeleteCul = { bg = C.red },
+
 	}
 end
 
--- lua/onehalf/groups/integrations/semantic_tokens.lua
+++ lua/onehalf/groups/integrations/semantic_tokens.lua
@@ -15,12 +15,12 @@ local M = {}
 function M.get(C, O)
 	return {
 		-- Identifiers
-		["@lsp.type.variable"] = {}, -- Identifiers that declare or reference a local or global variable
-		["@lsp.type.parameter"] = {}, -- Identifiers that declare or reference a function or method parameters
-		["@lsp.type.property"] = {}, -- Identifiers that declare or reference a member property, member field, or member variable
-		["@lsp.type.enumMember"] = {}, -- Identifiers that declare or reference an enumeration property, constant, or member
-		["@lsp.type.event"] = {}, -- Identifiers that declare an event property
-		["@lsp.type.namespace"] = {}, -- Identifiers that declare or reference a namespace, module, or package
+		["@lsp.type.variable"] = {},            -- Identifiers that declare or reference a local or global variable
+		["@lsp.type.parameter"] = {},           -- Identifiers that declare or reference a function or method parameters
+		["@lsp.type.property"] = {},            -- Identifiers that declare or reference a member property, member field, or member variable
+		["@lsp.type.enumMember"] = {},          -- Identifiers that declare or reference an enumeration property, constant, or member
+		["@lsp.type.event"] = {},               -- Identifiers that declare an event property
+		["@lsp.type.namespace"] = { fg = C.red }, -- Identifiers that declare or reference a namespace, module, or package
 
 		-- Literals
 		["@lsp.type.string"] = {}, -- Tokens that represent a string literal
@@ -29,18 +29,18 @@ function M.get(C, O)
 		["@lsp.type.number"] = {}, -- Tokens that represent a number literal
 
 		-- Types
-		["@lsp.type.class"] = {},   -- Identifiers that declare or represent a class type
-		["@lsp.type.enum"] = {},    -- Identifiers that declare or represent and
-		["@lsp.type.interface"] = {}, -- Identifiers that declare or reference an interface type
-		["@lsp.type.struct"] = {},  -- Identifiers that declare or reference a struct type
-		["@lsp.type.type"] = {},    -- Identifiers that declare or reference a type that is not covered above
+		["@lsp.type.class"] = {},       -- Identifiers that declare or represent a class type
+		["@lsp.type.enum"] = {},        -- Identifiers that declare or represent and
+		["@lsp.type.interface"] = {},   -- Identifiers that declare or reference an interface type
+		["@lsp.type.struct"] = {},      -- Identifiers that declare or reference a struct type
+		["@lsp.type.type"] = {},        -- Identifiers that declare or reference a type that is not covered above
 		["@lsp.type.typeParameter"] = {}, -- Identifiers that declare or reference a type parameter
-		["@lsp.type.decorator"] = {}, -- Identifiers that declare or reference decorator and annotations
+		["@lsp.type.decorator"] = {},   -- Identifiers that declare or reference decorator and annotations
 
 		-- Functions
 		["@lsp.type.function"] = {}, -- Identifiers that declare a function
 		["@lsp.type.method"] = {}, -- Identifiers that declare a member function or method
-		["@lsp.type.macro"] = {}, -- Identifiers that declare a macro
+		["@lsp.type.macro"] = {},  -- Identifiers that declare a macro
 		["@lsp.type.operator"] = {}, -- Tokens that represent an operator
 
 		-- Keywords
